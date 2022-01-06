190cd42f1ca19b3bd46cae990bc2a0e8c9302c79
Wes-Ray <wesleyray95@gmail.com>
2022-01-05 20:38:05 -0600

    added flags

--- grid_ctf_client/grid/grid.gd
+++ grid_ctf_client/grid/grid.gd
@@ -3,13 +3,14 @@ extends Node2D
 var cell_res = load("res://grid/grid_cell.png")
 
 var CELL_WIDTH := 32
-var GRID_WIDTH := 28
-var GRID_HEIGHT := 16
+var GRID_WIDTH := 28  # should be even number
+var GRID_HEIGHT := 15  # should be odd number
 
 onready var origin := get_node("origin")
 #onready var player := get_node("origin/player")
 
 onready var player_inst := preload("res://player.tscn")
+onready var flag_inst := preload("res://flag.tscn")
 
 var players = []  # local player objects, aligns with player_data array
 # list of player locations, aligns with players array, updated each tick
@@ -19,9 +20,14 @@ var player_coords = []
 var player_data = []
 enum Pd {TEAM = 0, ALIVE = 1}
 
+var flags = []  # local flag objects, aligned with flag_coords array
+var flag_coords = [Vector2(), Vector2()]  # flag coords, updated each tick
+var flag_attached_array = [-1, -1]  # flag linked to a player index or none (-1)
+
 func _ready():
 	generate_map()
-	players = generate_players()
+	generate_players()
+	generate_flags()
 
 
 func _process(delta):
@@ -45,15 +51,14 @@ func _process(delta):
 	elif Input.is_action_just_pressed("move_down"):
 		move_player(1, Vector2.DOWN)
 	
-	# check for collisions between players
-	# if player not on same team, and check what side they're on
-	
-	
-	
 	
 	# update grid reflection of player_cell_pos
 	for id in range(len(players)):
 		players[id].position = (player_coords[id] * CELL_WIDTH) + origin.position
+	
+	# draw flags
+	for id in range(len(flags)):
+		flags[id].position = (flag_coords[id] * CELL_WIDTH) + origin.position
 
 
 # this function should be used server-side, a remote function for the client will 
@@ -65,16 +70,19 @@ func move_player(player_id : int, dir : Vector2) -> void:
 	var new_y = clamp(new_pos.y, 0, GRID_HEIGHT-1)
 	new_pos = Vector2(new_x, new_y)  # update with clamp
 	
-	# check for collisions
+	# check for collisions with players
 	for other_p_id in range(len(player_coords)):
 		if player_data[player_id][Pd.ALIVE]:  # only collide if player is alive
 			if other_p_id != player_id:  # skip yourself
 				if player_coords[other_p_id] == new_pos:  # landed on other player
 					if player_data[other_p_id][Pd.ALIVE]:
 						print("COLLISION!!")
-						players[other_p_id].update_color(Color(1, 1, 1))  # change color of other player
+						players[other_p_id].update_color(Color(1, 1, 1, .3))  # change color of other player
 						player_data[other_p_id][Pd.ALIVE] = false
-					
+	
+	# check for collision with flag
+	for flag_id in range(len(flags)):
+		pass
 	
 	player_coords[player_id]= Vector2(new_pos)
 
@@ -91,9 +99,8 @@ func generate_map() -> void:
 			origin.add_child(new_cell)
 
 
-func generate_players(player_count : int = 2) -> Array:
+func generate_players(player_count : int = 2):
 	
-	var new_players = []
 	# generate player_data array SERVER
 	for x in range(player_count):
 		player_coords.append(Vector2(x*2, x*2))
@@ -108,8 +115,16 @@ func generate_players(player_count : int = 2) -> Array:
 		# create new player and add to list
 		var new_player = player_inst.instance()
 		origin.add_child(new_player)
-		new_players.append(new_player)
+		players.append(new_player)
+
+
+func generate_flags(player_count : int = 2):
+	for x in range(player_count):
+		var new_flag = flag_inst.instance()
+		origin.add_child(new_flag)
+		flags.append(new_flag)
 	
-	print(player_data)
-	
-	return new_players
+	# team 0 location
+	flag_coords[0] = Vector2(0, GRID_HEIGHT/2)
+	# team 1 location
+	flag_coords[1] = Vector2(GRID_WIDTH-1, GRID_HEIGHT/2)
