37c15ec8f99706c8ac1c5330ce4567367855ca83
Wes-Ray <42216488+Wes-Ray@users.noreply.github.com>
2022-02-26 16:11:25 -0600

    added ip selection

--- grid_ctf_client/splash_screen/splash_screen.gd
+++ grid_ctf_client/splash_screen/splash_screen.gd
@@ -5,11 +5,7 @@ var red_bb = "[color=red]RED[/color]"
 var blue_bb = "[color=blue]BLUE[/color]"
 
 
-
-
 func _process(delta):
-	if Input.is_action_just_pressed("ui_accept"):
-		get_tree().change_scene("res://grid/grid.tscn")
 	
 	if Input.is_action_just_pressed("move_left"):
 		MP.requested_team = 0  # red
@@ -19,3 +15,13 @@ func _process(delta):
 		MP.requested_team = 1  # blue
 		$red.bbcode_text = red_bb
 		$blue.bbcode_text = "[b][u]" + blue_bb + "[/u][/b]"
+
+
+func _on_Button_pressed():
+	
+	if $ip_input.text.count(".") == 3:
+		var x = $ip_input.text.split(".")
+		MP.SERVER_IP = str(x[0] + "." + x[1] + "." + x[2] + "." + x[3])
+		get_tree().change_scene("res://grid/grid.tscn")
+	else:
+		print("invalid ip")
